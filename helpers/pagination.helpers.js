1c6aa358f228572da2fb906b1c45c264685bf5ee
Santiago Iglesias <iglesias.arozena@gmail.com>
2020-04-05 19:36:47 +0200

    test pagination declare

--- helpers/pagination.helpers.js
+++ helpers/pagination.helpers.js
@@ -1,6 +1,4 @@
 const hbs = require('hbs');
-import * as paginate from 'node_modules/handlebars-paginate';
-
-// var paginate = require('handlebars-paginate');
+var paginate = require('handlebars-paginate');
 hbs.registerHelper('paginate', paginate);
  
